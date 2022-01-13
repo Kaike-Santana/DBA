5601225e2e6178b2a8633d2693ada1333bab2e32
Kaike Santana <kaike1010@hotmail.com.br>
2022-01-13 08:26:43 -0300

    Commit Final SQLServer

---
+++ Querys SQLServer/BIGGER_ATUALIZA.sql
@@ -0,0 +1,14 @@
+	USE    DB_REPORT
+
+	SELECT *
+             
+	FROM   TB_REL_HORA_HORA_CONTROLE_EXECUCAO
+
+/*
+UPDATE TB_REL_HORA_HORA_CONTROLE_EXECUCAO
+SET            DT_INICIO  =      NULL
+             , DT_FIM     =      NULL
+             , TP_STATUS  =      0
+*/
+
+
