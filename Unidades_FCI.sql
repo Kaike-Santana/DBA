e198e3bcbc78ad6a25846149c564b69f20fa5d8a
Kaike-Santana <97636434+Kaike-Santana@users.noreply.github.com>
2024-07-11 16:07:16 -0300

    Delete Unidades_FCI.sql

--- Unidades_FCI.sql
+++
@@ -1,8 +0,0 @@
-
-DROP TABLE [THESYS_HOMOLOGACAO].[dbo].[Unidades_FCI]
-SELECT 
-	   [id_unidade_fci]
-      ,[abreviatura]
-      ,[descricao]
-INTO [THESYS_HOMOLOGACAO].[dbo].[Unidades_FCI]
-FROM [THESYS_DEV].[dbo].[Unidades_FCI]
